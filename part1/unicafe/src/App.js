faca13462971909d9fd3b1fd12b0a67a6be247bf
Alphonzo Escolar <phonz.dev@gmail.com>
2022-03-09 14:19:25 +0800

    Finish exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,10 @@
 import { useState } from "react";
 
-const App = () => {
-	// save clicks of each button to its own state
-	const [good, setGood] = useState(0);
-	const [neutral, setNeutral] = useState(0);
-	const [bad, setBad] = useState(0);
-
-	const total = good + neutral + bad;
-	const average = ((good - bad) / total) * 1 || 0;
-	const positiveRate = (good / total) * 100;
+const Statistics = (props) => {
+	const { good, neutral, bad, total, average, positiveRate } = props;
 
 	return (
-		<div>
-			<h1>give feedback</h1>
-
-			<div>
-				<button onClick={() => setGood(good + 1)}>good</button>
-				<button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-				<button onClick={() => setBad(bad + 1)}>bad</button>
-			</div>
-
+		<>
 			<h2>statistics</h2>
 			<p>
 				good <span>{good}</span>
@@ -33,6 +18,38 @@ const App = () => {
 			<p>all {total}</p>
 			<p>average {average}</p>
 			<p>positive {positiveRate} %</p>
+		</>
+	);
+};
+
+const App = () => {
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+
+	const total = good + neutral + bad;
+	const average = ((good - bad) / total) * 1 || 0;
+	const positiveRate = (good / total) * 100 || 0;
+
+	return (
+		<div>
+			<h1>give feedback</h1>
+
+			<div>
+				<button onClick={() => setGood(good + 1)}>good</button>
+				<button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+				<button onClick={() => setBad(bad + 1)}>bad</button>
+			</div>
+
+			<Statistics
+				good={good}
+				neutral={neutral}
+				bad={bad}
+				total={total}
+				average={average}
+				positiveRate={positiveRate}
+			/>
 		</div>
 	);
 };
