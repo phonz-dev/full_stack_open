e5e0adeedd7b5666bf58875e41e17b09414bfcb3
Alphonzo Escolar <phonz.dev@gmail.com>
2022-03-09 14:04:51 +0800

    Finish exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,10 @@ const App = () => {
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 
+	const total = good + neutral + bad;
+	const average = ((good - bad) / total) * 1 || 0;
+	const positiveRate = (good / total) * 100;
+
 	return (
 		<div>
 			<h1>give feedback</h1>
@@ -26,6 +30,9 @@ const App = () => {
 			<p>
 				bad <span>{bad}</span>
 			</p>
+			<p>all {total}</p>
+			<p>average {average}</p>
+			<p>positive {positiveRate} %</p>
 		</div>
 	);
 };
