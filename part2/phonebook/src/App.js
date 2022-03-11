4d7d0633cab9d3fd29714c82d282f70b05f4f59c
Alphonzo Escolar <phonz.dev@gmail.com>
2022-03-11 21:03:59 +0800

    Finish exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,6 +6,12 @@ const App = () => {
 
 	const addPerson = (event) => {
 		event.preventDefault();
+
+		if (persons.some(({ name }) => name === newName)) {
+			alert(`${newName} is already added to phonebook`);
+			return;
+		}
+
 		const newPerson = { name: newName };
 		setPersons(persons.concat(newPerson));
 		setNewName("");
