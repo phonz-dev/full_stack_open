d15c4fc02d898bb2ad839e2b0f25aea10b1c9e87
Alphonzo Escolar <phonz.dev@gmail.com>
2022-03-11 17:38:59 +0800

    Finish exercise 2.1

---
+++ part2/courseinfo/src/components/Content.js
@@ -0,0 +1,8 @@
+import Part from "./Part";
+
+const Content = ({ parts }) =>
+	parts.map((part) => (
+		<Part key={part.id} name={part.name} exercises={part.exercises} />
+	));
+
+export default Content;
