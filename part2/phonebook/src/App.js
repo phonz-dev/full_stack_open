c18a1751d319515dd6f0e1bfa1826090a23fb9e5
Alphonzo Escolar <phonz.dev@gmail.com>
2022-03-12 13:52:57 +0800

    Finish exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect, useReducer } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "040 - 123456" },
-	]);
+	const [persons, setPersons] = useState([]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
 	const [filter, setFilter] = useState("");
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:3001/persons')
+			.then(response => setPersons(response.data))
+	},[])
 
 	const addPerson = (event) => {
 		event.preventDefault();
