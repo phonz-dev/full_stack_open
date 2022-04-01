c8bed3a7da0af1f5311b20815bd7128b407d8638
Alphonzo Escolar <phonz.dev@gmail.com>
2022-04-01 19:20:36 +0800

    Finish exercise 3.9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
+app.use(cors());
 
 morgan.token('data', (request, response) => {
   return JSON.stringify(request.body);
