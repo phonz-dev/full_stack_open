10146960440b91dfeeccc7700cde783220ecae86
Alphonzo Escolar <phonz.dev@gmail.com>
2022-04-02 14:28:52 +0800

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
 const app = express();
 
+app.use(express.static('build'));
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 app.use(cors());
