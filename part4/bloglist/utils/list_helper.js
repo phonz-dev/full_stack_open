d3784641f066ad28ac51979d3b6ac130d7da710e
Alphonzo Escolar <phonz.dev@gmail.com>
2022-04-06 09:39:44 +0800

    Finish exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogList) => {
+  const reducer = (total, {likes}) => {
+    return total + likes;
+  };
+
+  return blogList.reduce(reducer, 0);
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
