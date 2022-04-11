dab8ecdf34da44a921a54d5a510e1c1f19e3c304
Alphonzo Escolar <phonz.dev@gmail.com>
2022-04-11 20:21:30 +0800

    Finish exercise 4.14

--- part4/bloglist/tests/blog_list.test.js
+++ part4/bloglist/tests/blog_list.test.js
@@ -11,94 +11,118 @@ beforeEach(async () => {
 	await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-	await api
-		.get('/api/blogs')
-		.expect(200)
-		.expect('Content-Type', /application\/json/)
+describe('when there is initially some blogs saved', () => {
+	test('blogs are returned as json', async () => {
+		await api
+			.get('/api/blogs')
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+	})
+
+	test('all blogs are returned', async () => {
+		const blogs = await helper.blogsInDb()
+
+		expect(blogs).toHaveLength(helper.initialBlogs.length)
+	}, 100000)
+
+	test('blogs returned has an "id" property', async () => {
+		const blogs = await helper.blogsInDb()
+
+		for (let blog of blogs) {
+			expect(blog.id).toBeDefined()
+		}
+	})
 })
 
-test('all blogs are returned', async () => {
-	const blogs = await helper.blogsInDb()
+describe('addition of a new blog', () => {
+	test('succeeds with a valid data', async () => {
+		const blog = {
+			title: 'TDD harms architecture',
+			author: 'Robert C. Martin',
+			url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+			likes: 0,
+		}
 
-	expect(blogs).toHaveLength(helper.initialBlogs.length)
-}, 100000)
+		await api
+			.post('/api/blogs')
+			.send(blog)
+			.expect(201)
+			.expect('Content-Type', /application\/json/)
 
-test('blogs returned has an "id" property', async () => {
-	const blogs = await helper.blogsInDb()
+		const blogsAfterAdding = await helper.blogsInDb()
+		expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length + 1)
 
-	for (let blog of blogs) {
-		expect(blog.id).toBeDefined()
-	}
+		const titles = blogsAfterAdding.map(blog => blog.title)
+		expect(titles).toContain('TDD harms architecture')
+	})
+
+	test('if the "likes" property is undefined, it defaults to zero', async () => {
+		const blog = {
+			title: 'TDD harms architecture',
+			author: 'Robert C. Martin',
+			url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+		}
+
+		await api
+			.post('/api/blogs')
+			.send(blog)
+
+		const blogsAfterAdding = await helper.blogsInDb()
+		const recentlyAddedBlog = blogsAfterAdding[blogsAfterAdding.length - 1]
+
+		expect(recentlyAddedBlog.likes).toBe(0)
+		expect(recentlyAddedBlog.title).toBe('TDD harms architecture')
+	})
+
+	test('fails with status 400 if title and/or url is missing', async () => {
+		const blog = {
+			author: 'Edsger W. Dijkstra',
+			likes: 12,
+		}
+
+		await api
+			.post('/api/blogs')
+			.send(blog)
+			.expect(400)
+
+		const blogsAfterAdding = await helper.blogsInDb()
+		expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length)
+	})
 })
 
-test('a valid blog can be added', async () => {
-	const blog = {
-		title: 'TDD harms architecture',
-		author: 'Robert C. Martin',
-		url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-		likes: 0,
-	}
+describe('deletion of a blog', () => {
+	test('succeeds with valid id', async () => {
+		const blogsBeforeDeletion = await helper.blogsInDb()
+		const blogToDelete = blogsBeforeDeletion[0]
 
-	await api
-		.post('/api/blogs')
-		.send(blog)
-		.expect(201)
-		.expect('Content-Type', /application\/json/)
+		await api
+			.delete(`/api/blogs/${blogToDelete.id}`)
+			.expect(204)
 
-	const blogsAfterAdding = await helper.blogsInDb()
-	expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length + 1)
+		const blogsAfterDeletion = await helper.blogsInDb()
+		expect(blogsAfterDeletion).toHaveLength(helper.initialBlogs.length - 1)
 
-	const titles = blogsAfterAdding.map(blog => blog.title)
-	expect(titles).toContain('TDD harms architecture')
+		const titles = blogsAfterDeletion.map(blog => blog.title)
+		expect(titles).not.toContain(blogToDelete.title)
+	})
 })
 
-test('if the "likes" property is undefined, it defaults to zero', async () => {
-	const blog = {
-		title: 'TDD harms architecture',
-		author: 'Robert C. Martin',
-		url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-	}
+describe('updating a blog', () => {
+	test('succeeds with valid id', async () => {
+		const blogsAtStart = await helper.blogsInDb()
+		const blogToUpdate = blogsAtStart[0]
+		const newBlog = { ...blogToUpdate, likes: 30 }
 
-	await api
-		.post('/api/blogs')
-		.send(blog)
+		await api
+			.put(`/api/blogs/${blogToUpdate.id}`)
+			.send(newBlog)
+			.expect(200)
 
-	const blogsAfterAdding = await helper.blogsInDb()
-	const recentlyAddedBlog = blogsAfterAdding[blogsAfterAdding.length - 1]
+		const blogsAtEnd = await helper.blogsInDb()
+		const updatedBlog = blogsAtEnd[0]
 
-	expect(recentlyAddedBlog.likes).toBe(0)
-	expect(recentlyAddedBlog.title).toBe('TDD harms architecture')
-})
-
-test('blog without title and/or url is not added', async () => {
-	const blog = {
-		author: 'Edsger W. Dijkstra',
-		likes: 12,
-	}
-
-	await api
-		.post('/api/blogs')
-		.send(blog)
-		.expect(400)
-
-	const blogsAfterAdding = await helper.blogsInDb()
-	expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length)
-})
-
-test('a blog with a valid id can be deleted', async () => {
-	const blogsBeforeDeletion = await helper.blogsInDb()
-	const blogToDelete = blogsBeforeDeletion[0]
-
-	await api
-		.delete(`/api/blogs/${blogToDelete.id}`)
-		.expect(204)
-
-	const blogsAfterDeletion = await helper.blogsInDb()
-	expect(blogsAfterDeletion).toHaveLength(helper.initialBlogs.length - 1)
-
-	const titles = blogsAfterDeletion.map(blog => blog.title)
-	expect(titles).not.toContain(blogToDelete.title)
+		expect(newBlog).toEqual(updatedBlog)
+	})
 })
 
 afterAll(() => {
