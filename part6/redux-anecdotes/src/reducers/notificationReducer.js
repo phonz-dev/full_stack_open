769609f350f09eb05d6e450e9cae0c261079af79
Alphonzo Escolar <phonz.dev@gmail.com>
2022-04-24 14:48:55 +0800

    Finish exercise 6.10

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = 'SUCCESS'
+
+const notificationSlicer = createSlice({
+  name: 'notification',
+  initialState,
+  reducers: {
+    setNotification(state, action) {
+      return action.payload
+    }
+  }
+})
+
+export const { setNotification }  = notificationSlicer.actions
+export default notificationSlicer.reducer
